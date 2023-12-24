1bc00351624a28d02d96638976f23b1f287ac94f
Daniel Zhengyu Huang <huangdz@bicmr.pku.edu.cn>
2023-12-24 15:41:58 +0800

    update NACA ALE to turbulence flow

--- NACA/ALE/preprocess.sh
+++ NACA/ALE/preprocess.sh
@@ -2,6 +2,14 @@
 
 NF=4
 
+# decompose the mesh into 4 subdomains
 partnmesh sources/fluid.top $NF
+
+# preprocess distance to wall file with sower
 sower -fluid -mesh sources/fluid.top -dec sources/fluid.top.dec.${NF} -cpu $NF -cluster $NF -output data/OUTPUT
 
+# generate the distance to the wall file
+cd2tet -mesh sources/fluid.top -output sources/fluid.sinus
+
+# preprocess distance to wall file with sower
+sower -fluid -split -mesh data/OUTPUT.msh -con data/OUTPUT.con -cluster ${NF} -result sources/fluid.sinus.dwall -ascii -output data/OUTPUT.dwall
--- NACA/ALE/simulations/case1/run.Steady.sh
+++ NACA/ALE/simulations/case1/run.Steady.sh
@@ -9,11 +9,20 @@ sower -fluid -merge -con ../../data/OUTPUT.con -mesh ../../data/OUTPUT.msh \
     
 sower -fluid -merge -con ../../data/OUTPUT.con -mesh ../../data/OUTPUT.msh \
 	-result results.Steady/Pressure.bin -output postpro.Steady/Pressure
+    
+sower -fluid -merge -con ../../data/OUTPUT.con -mesh ../../data/OUTPUT.msh \
+	-result results.Steady/PressureCoefficient.bin -output postpro.Steady/PressureCoefficient
+
+sower -fluid -merge -con ../../data/OUTPUT.con -mesh ../../data/OUTPUT.msh \
+	-result results.Steady/SkinFriction.bin -output postpro.Steady/SkinFriction
+
 
 
 xp2exo ../../sources/fluid.top fluid_solution.exo \
                    ../../sources/fluid.top.dec.${NF} \
-                   postpro.Steady/Mach.xpost postpro.Steady/Pressure.xpost  
+                   postpro.Steady/Mach.xpost postpro.Steady/Pressure.xpost \
+                   postpro.Steady/PressureCoefficient.xpost postpro.Steady/SkinFriction.xpost
+                   
 
 
 
