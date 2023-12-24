72d4f9d34f047fc4ae5f2d4cd343a6a9214c4809
Daniel Zhengyu Huang <huangdz@bicmr.pku.edu.cn>
2023-12-24 18:06:45 +0800

    add NACA EBM

--- NACA/EBM/simulations/case1/run.Steady.sh
+++ NACA/EBM/simulations/case1/run.Steady.sh
@@ -11,12 +11,12 @@ sower -fluid -merge -con ../../data/OUTPUT.con -mesh ../../data/OUTPUT.msh \
 	-result results.Steady/Pressure.bin -output postpro.Steady/Pressure
 
 
-xp2exo ../../sources/fluid.top fluid_solution.exo \
+xp2exo ../../sources/fluid.top fluid_solution.Steady.exo \
                    ../../sources/fluid.top.dec.${NF} \
                    postpro.Steady/Mach.xpost postpro.Steady/Pressure.xpost  
 
 
-xp2exo ../../sources/structure.top structure_solution.exo \
+xp2exo ../../sources/structure.top structure_solution.Steady.exo \
                    postpro.Steady/EmbeddedSurfacePressureCoefficient.xpost
 
 
