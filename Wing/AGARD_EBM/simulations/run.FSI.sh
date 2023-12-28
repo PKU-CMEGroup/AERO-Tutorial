339e81349053e6f5a8d8c15e649386fb5c76536c
Daniel Zhengyu Huang <huangdz@bicmr.pku.edu.cn>
2023-12-28 19:55:19 +0800

    fix typos

--- Wing/AGARD_EBM/simulations/run.FSI.sh
+++ Wing/AGARD_EBM/simulations/run.FSI.sh
@@ -5,17 +5,3 @@ NS=1
 # run Steady state
 mpirun -n $NF aerof.opt FluidFile.FSI : -n $NS aeros StructureFile.FSI |& tee log.FSI.out
 
-
-
-#postprocess fluid solution
-sower -fluid -merge -con ../data/fluidmodel.con -mesh ../data/fluidmodel.msh -result results.FSI/Pressure.bin -output postpro.FSI/Pressure
-sower -fluid -merge -con ../data/fluidmodel.con -mesh ../data/fluidmodel.msh -result results.FSI/Mach.bin -output postpro.FSI/Mach
-
-
-# Convert fluid outputs to .exo format
-xp2exo ../sources/fluid.top     fluid_solution.FSI.exo     postpro.FSI/Pressure.xpost postpro.FSI/Mach.xpost 
-xp2exo ../sources/embeddedSurface.top  embeddedSurface.FSI.exo  postpro.FSI/EmbeddedSurfaceDisplacement.xpost postpro.FSI/EmbeddedSurfacePressureCoefficient.xpost
-
-xp2exo ../sources/Structure.top  structure_solution.FSI.exo postpro.FSI/structure.GDISPLAC.xpost 
-
-
